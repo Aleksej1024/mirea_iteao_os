0ae356979d0576242b456ccda6c1628ea2604fba
Aleksej1024 <aleksejpropastin@gmail.com>
2024-05-08 09:57:03 +0300

    rc4 work with files now!

--- rc4_gummy/main.cpp
+++ rc4_gummy/main.cpp
@@ -1,36 +1,12 @@
 #include <iostream>
 #include <vector>
+#include <fstream>
 #include "rc4.h"
 
-int main() {
-    std::string key = "Thisvhjvu";
-    std::string plain_text = "fbyvbhs";
-    std::cin>>key;
-    std::cin>>plain_text;
-
-    // Генерация ключа
+std::string code(std::string key,std::string cipher_text){
+    
+    std::string result;
     std::vector<unsigned char> key_vec(key.begin(), key.end());
-    set_key(key_vec);
-
-    // Кодирование текста
-    std::vector<unsigned char> cipher_text;
-    for (unsigned char c : plain_text) {
-        //std::cout<<(static_cast<int>(c))<<" ";
-        cipher_text.push_back(static_cast<unsigned char >(c ^ g()));
-    }
-    std::cout<<std::endl;
-    // Вывод закодированного текста
-    //std::cout << "Закодированный текст: " << cipher_text << std::endl;
-    for(unsigned char elem : cipher_text)
-    {
-        std::cout<<static_cast<unsigned char>(elem)<< " ";
-    }
-    std::cout<<std::endl;
-    // Очистка памяти
-    fin();
-////////////////////////////////////////////////////////////////////////////////////////////
-    //Проверка при помощи Декодирование текста
-
     // Генерация ключа
     set_key(key_vec);
 
@@ -43,13 +19,59 @@ int main() {
     }
     for(unsigned char elem : uncipher_text)
     {
-        std::cout<<static_cast<unsigned char >(elem)<< " ";
+        result+=static_cast<char>(elem);
+    }
+    std::cout<<result<<std::endl;
+    fin();
+    return result;
+}
+std::string decode(std::string key, std::string plain_text){
+    std::vector<unsigned char> key_vec(key.begin(), key.end());
+    set_key(key_vec);
+    std::string result="";
+        // Кодирование текста
+    std::vector<unsigned char> cipher_text;
+    for (unsigned char c : plain_text) {
+        //std::cout<<(static_cast<int>(c))<<" ";
+        cipher_text.push_back(static_cast<unsigned char >(c ^ g()));
     }
     std::cout<<std::endl;
-
-//////////////////////////////////////////////////////////////////////////////////////////////////
-    // Очистка памяти
+    // Вывод закодированного текста
+    //std::cout << "Закодированный текст: " << cipher_text << std::endl;
+    for(unsigned char elem : cipher_text)
+    {
+        //std::cout<<static_cast<unsigned char>(elem)<< " ";
+        result+=static_cast<unsigned char>(elem);
+    }
+ 
     fin();
+    return result;
+}
 
+
+int main() {
+    std::ofstream file("crypt.txt", std::ios::out | std::ios::trunc);
+    std::string key = "Thisvhjvu";
+    std::string plain_text = "fbyvbhs";
+    std::string flag;
+    std::cout<<"Введите ключ"<<std::endl;
+    std::cin>>key;
+    std::cout<<"Расшифровать из decrypt.txt или зашифровать в файл  crypt.txt? 1 or 0"<<std::endl;
+    std::cin>>flag;
+    if(flag=="1"){
+        std::ifstream infile("decrypt.txt");
+        if (infile.is_open()) {
+            std::string line;
+            std::getline(infile, line);
+        std::cout<<decode(key, line);
+        infile.close();
+    }
+    }
+    if(flag=="0"){
+        std::cout<<"Введите текст"<<std::endl;
+        std::cin>>plain_text;
+        file<<code(key,plain_text);
+    }
+    file.close();
     return 0;
 }
